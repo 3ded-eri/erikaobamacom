737e3284160b3361b31263a0f012b2a338a6b276
3ded-eri <120373336+3ded-eri@users.noreply.github.com>
2025-02-06 13:00:25 +0900

    Add contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://3ded-eri.github.io',
+  base: 'erikaobamacom',
+  integrations: [preact(), tailwind()],
+  i18n: {
+    locales: ["en", "ja"],
+    defaultLocale: "en",
+      routing: {
+        prefixDefaultLocale: true
+    }
+  }
+});
