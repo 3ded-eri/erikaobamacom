1531798706935868404e00777a303dca5ddcce59
3ded-eri <120373336+3ded-eri@users.noreply.github.com>
2025-02-06 17:30:40 +0900

    Change the default locale setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     locales: ["en", "ja"],
     defaultLocale: "en",
       routing: {
-        prefixDefaultLocale: false
+        prefixDefaultLocale: true
     }
   }
 });
