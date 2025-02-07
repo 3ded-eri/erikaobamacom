8dceba78f2e20ff6ef720f241f2a4678a6111dba
3ded-eri <120373336+3ded-eri@users.noreply.github.com>
2025-02-08 07:51:41 +0900

    Change site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://erikaobama.com',
+  base: '/',
   integrations: [preact(), tailwind()],
   i18n: {
     locales: ["en", "ja"],
